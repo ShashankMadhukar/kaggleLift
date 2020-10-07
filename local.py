d735d672379ae8315722290964e731f312d5ba90
hugzee3 <skandaavadhani3@gmail.com>
2020-10-07 22:05:53 +0200

    kaggleLift: Initialized the repo with the reference examples from the LiftGitHub.

---
+++ local.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = os.path.dirname('__name__')
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+PREDICTION_DS_DIR = os.path.join(DATA_DIR, 'prediction_ds')
+PERCEPTION_DS_DIR = os.path.join(DATA_DIR, 'perception_ds')
